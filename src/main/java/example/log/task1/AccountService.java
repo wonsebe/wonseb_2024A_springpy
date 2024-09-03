363270bd5e0baa0788b4eabdd50aecaa6daa9d6a
wonsebe <wonsb03129@gmail.com>
2024-09-03 11:38:13 +0900

    오류?

--- src/main/java/example/log/task1/AccountService.java
+++ src/main/java/example/log/task1/AccountService.java
@@ -1,18 +1,22 @@
 package example.log.task1;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Log4j2
 @Service
 public class AccountService {
 
     @Autowired private  AccountDao accountDao;
 
+
     //가계부 등록
     public int create(AccountDto accountDto){
-        return accountDao.create(accountDto);
+        int num =  accountDao.create( accountDto );
+        if(num==1){ log.info("Account create"); }
+        return 1;
     }
     //전체출력
     public List<AccountDto>allPrint(){
@@ -21,10 +25,17 @@ public class AccountService {
 
     //수정
     public boolean  update(AccountDto accountDto){
-        return accountDao.update(accountDto);
+        System.out.println("accountDto = " + accountDto);
+        boolean result = accountDao.update( accountDto );
+        if( result ){ log.info("Account updated"); }
+        return result;
     }
     //삭제
     public int  delete(int hno){
-        return accountDao.delete(hno);
+        int num =  accountDao.delete( hno );
+        if(num==1){ log.info("Account deleted"); }
+        return 1;
     }
-}
+    }
+
+
