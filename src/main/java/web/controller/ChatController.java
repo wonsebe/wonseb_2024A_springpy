1809c90469439d67e91c63325bf58c486aeb671e
wonsebe <wonsb03129@gmail.com>
2024-09-09 18:09:56 +0900

    9월9일 커밋

---
+++ src/main/java/web/controller/ChatController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component //해당 클래스를 스프링 컨테이너 빈 등록
+public class ChatController extends TextWebSocketHandler {
+
+    //1. 클라이언트가 서버 웹 소켓에 접속 요청 했을 때
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        System.out.println("session = " + session);
+        System.out.println("[서버웹소켓 측] JS웹소켓이 들어옴");
+    }
+    //2. 클라이언트가 서버 웹소켓에 접속 끊었을 때
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        System.out.println("session = " + session);
+        System.out.println("[서버웹소켓 측] JS웹소켓이 나감");
+    }
+
+
+}
